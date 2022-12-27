1bdfb72393c172a237909e2b3d6f91392153bcee
delluser <delluser@LAPTOP-6N2GONVC>
2022-12-27 18:38:29 +0530

    selenium windows

--- src/main/java/umberlla/Windows.java
+++ src/main/java/umberlla/Windows.java
@@ -13,6 +13,7 @@ public class Windows {
 	public static void setWindow() {
 		WebDriver driver=new ChromeDriver();
 		driver.get("https://www.leafground.com/window.xhtml");
+		String oldwindow=driver.getWindowHandle();
 		WebElement element=driver.findElement(By.xpath("//button[@id='j_idt88:new']"));
 		element.click();
 		Set<String> handles=driver.getWindowHandles();
@@ -21,8 +22,21 @@ public class Windows {
 		}
 		WebElement editbox=driver.findElement(By.xpath("//*[text()='Daily Quiz']/preceding-sibling::*[1]"));
 		editbox.click();
+		driver.close();
+		driver.switchTo().window(oldwindow);
+		WebElement multiplewindow=driver.findElement(By.xpath("//*[text()='Open Multiple']"));
+		multiplewindow.click();
+		int countwindow=driver.getWindowHandles().size();
+		System.out.println(countwindow);
+		WebElement multiple=driver.findElement(By.xpath("//*[text()=\"Close Windows\"]"));
+		multiple.click();
+	Set<String> mwindows=	driver.getWindowHandles();
+	for (String windows : mwindows) {
+		if(!windows.equals(oldwindow)) {
+			driver.switchTo().window(windows);
+			driver.close();
+		}
 		
-		
-		
+	}
 	}
 }
